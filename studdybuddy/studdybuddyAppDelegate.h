ee0da14ca818954ffe13086bb60d51bfc8d236d7
dquail <quail.david@gmail.com>
2011-10-01 11:25:42 -0700

    Initial Commit

---
+++ studdybuddy/studdybuddyAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  studdybuddyAppDelegate.h
+//  studdybuddy
+//
+//  Created by David Quail on 10/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface studdybuddyAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
